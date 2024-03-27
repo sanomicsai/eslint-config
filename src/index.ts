84826b08f76c2de2863383857731c1dcba144f6f
Ted Xu <hi@xzd.me>
2024-03-27 10:16:53 +0800

    fix: explictly add return type annotation of eslintConfig to prevent build error

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import { defu } from 'defu'
 import { configs } from './configs'
 
 import type { Awaitable, FlatConfigItem, OptionsConfig, UserConfigItem } from '@antfu/eslint-config'
+import type { FlatConfigPipeline } from 'eslint-flat-config-utils'
 
 export default function eslintConfig(
     options?: OptionsConfig & FlatConfigItem,
     ...userConfigs: Awaitable<UserConfigItem | UserConfigItem[]>[]
-) {
+): FlatConfigPipeline<UserConfigItem> {
     return antfu(
         defu(options, {
             stylistic: {
